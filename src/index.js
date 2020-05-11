119f546a3e5deb9d8a74412a664aa039392fe23f
Sachin Saini <iamsainisachin@gmail.com>
2020-05-11 18:26:04 +0530

    add context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
+const jwt = require('jsonwebtoken');
 const typeDefs = require('./schema');
 const UserAPI = require('./datasources/user');
 const store = require('./db');
 const resolvers = require('./resolvers');
 
 const dataSources = () => ({ userApi: new UserAPI({ store }) });
-
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const context = ({ req }) => {
+  const token = req.headers && req.headers.authorization || '';
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decoded);
+};
+const server = new ApolloServer({
+  context, typeDefs, resolvers, dataSources,
+});
 server.listen().then(({ url }) => {
   console.log(`server ready at ${url}`);
 });
